cbf33f13521b166ccea19ba6bc6ac86fc0baca9f
Alvis Chan <u7287079@edu.anu.au>
2023-01-08 16:06:42 +1100

    Added filter for sections. Currently statically defined.

--- site/Back.js
+++ site/Back.js
@@ -1,18 +1,25 @@
+//Listens for csv file change
 const input = document.querySelector('#file-input');
 const output = document.querySelector('#output');
 let data;
 
 input.addEventListener('change', () => {
-const file = input.files[0];
+  output.innerHTML = ''; // clear the output element
 
-if (file) {
+  const file = input.files[0];
+
+  if (file) {
     const reader = new FileReader();
     reader.readAsText(file);
 
     reader.onload = () => {
-    try {
+      try {
         const csvData = reader.result;
         data = Papa.parse(csvData).data;
+
+        // get the unique values from the second column of the data array
+        const values = data.map(row => row[1]);
+        const uniqueValues = Array.from(new Set(values));
 
         // build an HTML table from the data
         const table = document.createElement('table');
@@ -41,15 +48,60 @@ if (file) {
         // append thead and tbody to the table and the table to the output element
         table.appendChild(thead);
         table.appendChild(tbody);
-        output.innerHTML = '';
         output.appendChild(table);
-    } catch (error) {
-        output.innerHTML = `<p>Error: ${error.message}</p>`;
+        } catch (error) {
+          output.innerHTML = `<p>Error: ${error.message}</p>`;
+        }
+      };
+  
+      reader.onerror = () => {
+        output.innerHTML = '<p>Error: Failed to read file.</p>';
+      };
     }
-    };
+  });
+  
+  const select = document.querySelector('#filter-select');
+  
+  select.addEventListener('change', () => {
+    const value = select.value;
+    
+    
+    // update the table rows based on the selected value
+    const rows = Array.from(document.querySelectorAll('tbody tr'));
+    rows.forEach(row => {
+      const sectionCell = row.querySelector('td:nth-child(2)');
+      if ('All' !== value && sectionCell.textContent !== value) {
+        row.style.display = 'none';
+      } else {
+        row.style.display = '';
+      }
+    });
+  });
+  
+  
 
-    reader.onerror = () => {
-    output.innerHTML = '<p>Error: Failed to read file.</p>';
-    };
-}
+
+//listens for column change
+const filterCol = document.querySelector('#filterCol-form');
+
+filterCol.addEventListener('change', event => {
+  event.preventDefault();
+
+  // get the search query from the input element
+  const input = document.querySelector('#search-input');
+  const query = input.value.trim().toLowerCase();
+
+  // search the tables for rows that match the query
+  const tables = Array.from(document.querySelectorAll('table'));
+  tables.forEach(table => {
+    const rows = Array.from(table.querySelectorAll('tbody tr'));
+    rows.forEach(row => {
+      const cells = Array.from(row.querySelectorAll('td'));
+      if (!cells.some(cell => cell.textContent.toLowerCase().includes(query))) {
+        row.style.display = 'none';
+      } else {
+        row.style.display = '';
+      }
+    });
+  });
 });
